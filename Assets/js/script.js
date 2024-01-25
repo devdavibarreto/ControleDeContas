4825c3955ca149ce62eafd21e8235e7179fe257f
Davi Barreto <davibarretojkda@gmail.com>
2024-01-24 23:22:33 -0300

    Primeiros passos do script

---
+++ Assets/js/script.js
@@ -0,0 +1,4 @@
+const contas = document.querySelector(".contas");
+const valor = document.querySelector(".valor")
+const addTarefa = document.querySelector(".addtarefa")
+const registroDeContas = document.querySelector(".registrodecontas")
