dd4af91f933b8212d2dd97b5d34b101b64b4e24f
Davi Barreto <davibarretojkda@gmail.com>
2024-01-27 01:16:32 -0300

    Adicionando logica do botão para apagar as tarefas

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -24,6 +24,13 @@ function LimpaInput(){ // Para limpar o input e volta o foco pra ele
     contas.focus(); //Voltar o foco pra ele
 }
 
+function criaBotaoApagar(paragrafo1){ // Recebe o li
+    paragrafo1.innerText += ' ' // Para da um espaço
+const botaoApagar = document.createElement('button');
+botaoApagar.innerText= 'Apagar' ; //Criando o botão
+botaoApagar.setAttribute('class', 'apagar') // Definir a classe e depois o nome da classe
+botaoApagar.setAttribute('title', 'apagar esta tarefa')
+paragrafo1.appendChild(botaoApagar);}
 
 function regTarefas(contasInput,valorInput){
     const paragrafo = criaP()
@@ -32,8 +39,27 @@ function regTarefas(contasInput,valorInput){
     paragrafo1.innerHTML = valorInput;
     registroDeContas.appendChild(paragrafo,paragrafo1);
     LimpaInput()
+    criaBotaoApagar(paragrafo)
+    atualizarContas()
 }
 
+document.addEventListener('click', function(e){
+    const apagar = e.target;
 
+    if (apagar.classList.contains('apagar')){
+        apagar.parentElement.remove();
+        atualizarContas();
+    }
+})
 
+function atualizarContas(){
+    const paragrafosComContas = registroDeContas.querySelectorAll('p');
+    const listaDeContas= []
+
+    for (let registroDeContas  of paragrafosComContas){
+        let  contas = registroDeContas.innerText;
+        contas = contas.replace('Apagar', '').trim()
+        listaDeContas.push(registroDeContas)
+    }
+}
 
