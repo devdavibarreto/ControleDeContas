ee512cb59b63dd51d5628d9bfcf1861fe28f25ff
Davi Barreto <davibarretojkda@gmail.com>
2024-01-27 00:35:39 -0300

    Dia 3

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,3 +2,44 @@ const contas = document.querySelector(".contas");
 const valor = document.querySelector(".valor")
 const addTarefa = document.querySelector(".addtarefa")
 const registroDeContas = document.querySelector(".registrodecontas")
+const total = document.querySelector(".total");
+
+function criaP(){
+    const paragrafo = document.createElement('p')
+    return paragrafo;
+}
+
+// function criaPDoDinheiro(){
+//     const dinheiro = document.createElement('p')
+//     return dinheiro;
+// }
+
+
+
+
+addTarefa.addEventListener('click', function(e){
+    if(!contas.value + !valor.value)return;
+    regTarefas(contas.value +  " R$ " + valor.value);
+        
+   
+});
+
+function LimpaInput(){ // Para limpar o input e volta o foco pra ele
+    contas.value = ''; //limpar
+    valor.value = '';
+    contas.focus(); //Voltar o foco pra ele
+}
+
+
+function regTarefas(contasInput,valorInput){
+    const paragrafo = criaP()
+    const paragrafo1 = criaP()
+    paragrafo.innerText = contasInput;
+    paragrafo1.innerHTML = valorInput;
+    registroDeContas.appendChild(paragrafo,paragrafo1);
+    LimpaInput()
+}
+
+
+
+
